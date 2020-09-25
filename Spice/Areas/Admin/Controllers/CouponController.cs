d507e42024a47928250c67169fcd0a28fcdfe330
Wojtek Krol <wojtekmkrol@gmail.com>
2020-09-25 17:35:39 +0200

    Added Edit View and Control to Coupon

--- Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
 using Spice.Models;
@@ -25,11 +27,13 @@ namespace Spice.Areas.Admin.Controllers
             return View(await _db.Coupon.ToListAsync());
         }
 
+        //GET - CREATE
         public IActionResult Create()
         {
             return View();
         }
 
+        //POST - CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Coupon coupons)
@@ -38,8 +42,51 @@ namespace Spice.Areas.Admin.Controllers
             {
                 var files = HttpContext.Request.Form.Files;
 
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
 
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupons.Picture = p1;
+                }
+                _db.Coupon.Add(coupons);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
+            return View(coupons);
         }
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _db.Coupon.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+
+        }
+
+        //POST - EDIT
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(Coupon coupon)
+        //{
+        //}
     }
 }
